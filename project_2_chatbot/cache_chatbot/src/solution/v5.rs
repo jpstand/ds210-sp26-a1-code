0203586cbddaacdaa637fbe7337fc132284675a7
Philipp <jstandring7@gmail.com>
2026-03-24 20:56:07 -0400

    added get_history

--- project_2_chatbot/cache_chatbot/src/solution/v5.rs
+++ project_2_chatbot/cache_chatbot/src/solution/v5.rs
@@ -43,14 +43,36 @@ impl ChatbotV5 {
             None => {
                 println!("get_history: {username} is not in the cache!");
                 // TODO: The cache does not have the chat. What should you do?
-                // Your code goes here.
-                return Vec::new();
+                match file_library::load_chat_session_from_file(filename) {
+                    None => {
+                        return Vec::new();
+                    },
+                    Some(session) => {
+                        let history = session.history();
+                        let mut temp = Vec::new();
+                    for i in history{
+                        temp.push(i.content().to_string()); // pushing the history of the Vec
+                    }
+                        temp.remove(0); // removed first index, first index caused misalignment when reopened.             
+                        println!("{:?}", temp);
+                    
+                    return temp; // return the history
+                    }
+        }
             }
             Some(chat_session) => {
                 println!("get_history: {username} is in the cache! Nice!");
                 // TODO: The cache has this chat. What should you do?
-                // Your code goes here.
-                return Vec::new();
+                let user_session = chat_session.session().unwrap(); // get the session 
+                let history = user_session.history(); // get the sesion history
+                let mut temp = Vec::new();
+                for i in &history{
+                    temp.push(i.content().to_string()); // pushing the history of the Vec
+                }
+                temp.remove(0); // removed first index, first index caused misalignment when reopened.             
+                println!("{:?}", temp);
+                    
+                return temp; // return the history
 
             }
         }
