ccccb5dbe66dd789179d94f00f07f1bc966b32ba
Ricky <rcui1@bu.edu>
2026-03-29 17:50:08 -0400

    filter_dataset

--- project_3_client_server_analytics/analytics_lib/src/solution.rs
+++ project_3_client_server_analytics/analytics_lib/src/solution.rs
@@ -3,8 +3,45 @@ use crate::dataset::{ColumnType, Dataset, Value, Row};
 use crate::query::{Aggregation, Condition, Query};
 
 pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
-    todo!("Implement this!");
+    let iterator = dataset.iter(); //using iter rather than into_iter becuase we only need read permissions.
+    let columns = dataset.columns(); // copy the structure of the OG dataset.
+    let mut filtered_dataset = Dataset::new(columns.clone()); // data set that will be filled and returned
+    
+    for row in iterator{ // check each row.
+       // because Condition has so many possible filters we must match each case. 
+       // recursive function bec of the way it is set up in dataset 
+        fn check(r: &Row, f: &Condition, ds: &Dataset) -> bool{ //helper function
+            match f{
+                // string = the name of the column we need to compare the value of
+                // value = this is the element we need to compare with.   
+                Condition::Equal(column_name ,value)=>{ 
+                    let col_index = ds.column_index(column_name); //finding the column index of the column that matters
+                    let self_value= r.get_value(col_index); // getting the value at that col_index and row
+                    
+                    return self_value == value; //compare the two values
+                }
+                Condition::Not(a,)=>{ 
+                    return !check(r,a,ds);
+                }
+                Condition::And(a,b)=>{
+                    return check(r,a,ds) && check(r,b,ds);
+                }
+                Condition::Or(a,b )=>{
+                    return check(r,a,ds) || check(r,b,ds);
+                }
+            }
+        }
+        
+        if check(row,filter,dataset){ // if it passes the filter, add it to the new dataset. 
+            filtered_dataset.add_row(row.clone());
+        }
+       
+    }
+    return filtered_dataset;
 }
+
+    //looks at the rows in the given dataset, checks whether they meet the filter condition, and return a new Dataset that only contains matching rows.
+
 
 pub fn group_by_dataset(dataset: Dataset, group_by_column: &String) -> HashMap<Value, Dataset> {
     todo!("Implement this!");
